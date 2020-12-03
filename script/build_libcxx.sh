78c6a2e7a99c6010cfa7cfb0b7a1756218ed1caf
Michael Unknown <michael_knoerzer@gmx.net>
2020-12-03 21:43:16 +0100

    test CI

--- script/build_libcxx.sh
+++ script/build_libcxx.sh
@@ -2,32 +2,31 @@
 
 set -e
 
-echo "building libc++/libc++abi 11 from source"
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-11.0.0.src.tar.xz
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/libcxx-11.0.0.src.tar.xz
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/libcxxabi-11.0.0.src.tar.xz
+echo "building the latest libcxx from source..."
+git clone https://github.com/llvm/llvm-project
 
-mkdir llvm-source
-mkdir llvm-source/projects
-mkdir llvm-source/projects/libcxx
-mkdir llvm-source/projects/libcxxabi
+INSTALL_TARGET=`pwd`/local
+mkdir "${INSTALL_TARGET}"
+cd llvm-project
 
-tar -xf llvm-11.0.0.src.tar.xz -C llvm-source --strip-components=1
-tar -xf libcxx-11.0.0.src.tar.xz -C llvm-source/projects/libcxx --strip-components=1
-tar -xf libcxxabi-11.0.0.src.tar.xz -C llvm-source/projects/libcxxabi --strip-components=1
+cmake \
+  -S libcxx \
+  -B buildcxx \
+  -D CMAKE_C_COMPILER=${CC} \
+  -D CMAKE_CXX_COMPILER=${CXX} \
+  -D "CMAKE_INSTALL_PREFIX=${INSTALL_TARGET}" \
+  -D CMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build buildcxx -j2 -v
+cmake --build buildcxx --target install
 
-TARGET=`pwd`/llvm
-mkdir "${TARGET}"
-mkdir llvm-build
-cd llvm-build
-
-cmake -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} \
-        -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="${TARGET}" \
-        -DLIBCXX_ABI_UNSTABLE=ON \
-        ../llvm-source
-make cxx -j4 VERBOSE=1
-# we install into a local dir, no sudo needed
-make install-cxxabi install-cxx
-
+cmake \
+  -S libcxxabi \
+  -B buildcxxabi \
+  -D CMAKE_C_COMPILER=${CC} \
+  -D CMAKE_CXX_COMPILER=${CXX} \
+  -D "CMAKE_INSTALL_PREFIX=${INSTALL_TARGET}" \
+  -D CMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build buildcxxabi -j2 -v
+cmake --build buildcxxabi --target install
 
 exit 0
